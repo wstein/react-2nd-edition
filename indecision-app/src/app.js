c7ab2ee381fc4ec4603aff63b4b66d719612e805
Werner Stein <werner.stein@gmail.com>
2022-04-09 22:13:17 +0200

    10. Exploring JSX

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,16 @@
 console.log('App.js is running!');
 
-const template = <h1>This is JSX from App.js!</h1>;
+const template = (
+    <div>
+        <h1>This is JSX from App.js</h1>
+        <p>this is some text...</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
+
 const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
