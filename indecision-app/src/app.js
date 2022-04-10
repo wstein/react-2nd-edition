b9f845cd0ebc6655173e922ef00c4c4f1eb9e813
Werner Stein <werner.stein@gmail.com>
2022-04-10 13:29:28 +0200

    12. Conditional Rendering in JSX

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,33 +2,53 @@ console.log('App.js is running!');
 
 
 const app = {
-    title :'Indecision App',
-    subtitle: 'Here is some info...'
+    title: 'Indecision App',
+    subtitle: 'Here is some info...',
+    options: ['One', 'Two', "Three", "Juhu!!"]
+}
+
+function renderOptions(options) {
+    if (options && options.length > 0) {
+        return (
+            <div>
+                <p>Here are your options: </p>
+                <ol> {
+                    options.map((option, id) => <li key={id}>Item {option}</li>)
+                } </ol>
+            </div>)
+            ;
+    } else {
+        return <p>No options</p>
+    }
 }
 
 const template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
-        <ol>
-            <li>Item One</li>
-            <li>Item Two</li>
-        </ol>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {renderOptions(app.options)}
     </div>
 );
 
-const user =
-    {
-        name: 'Werner',
-        age: 54,
-        location: 'Ludwigsburg'
-    };
+const user = {
+    name: 'Werner',
+    age: 54,
+    location: 'Ludwigsburg'
+};
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {user.location}</p>;
+    }
+    // if location is unknown getLocation returns undefined
+    // e.g. return undefined;
+}
 
 const templateTwo = (
     <div>
-        <h1>{user.name}!</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ? user.name : "Anonymous"}!</h1>
+        {user.age && user.age >= 18 ? <p>Age: {user.age}</p> : false}
+        {getLocation(user.location)}
     </div>
 );
 
