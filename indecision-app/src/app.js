4897a11537677223caa53f5864f480492c60f3b9
Werner Stein <werner.stein@gmail.com>
2022-04-11 22:58:19 +0200

    18. Forms and Inputs

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,88 +4,53 @@ console.log('App.js is running!');
 const app = {
     title: 'Indecision App',
     subtitle: 'Here is some info...',
-    options: ['One', 'Two', "Three", "Juhu!!"]
+    options: []
 }
 
 function renderOptions(options) {
     if (options && options.length > 0) {
-        return (
-            <div>
-                <p>Here are your options: </p>
-                <ol> {
-                    options.map((option, id) => <li key={id}>Item {option}</li>)
-                } </ol>
-            </div>)
-            ;
+        return (<div>
+            <p>Here are your options: </p>
+            <button onClick={removeAll}>Remove All</button>
+            <ol> {options.map((option, id) => <li key={id}>Item {option}</li>)} </ol>
+        </div>);
     } else {
         return <p>No options</p>
     }
 }
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        {renderOptions(app.options)}
-    </div>
-);
+function onFormSubmit(e) {
+    e.preventDefault();
 
-const user = {
-    name: 'Werner',
-    age: 54,
-    location: 'Ludwigsburg'
-};
-
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {user.location}</p>;
+    const option = e.target.elements.option.value;
+    console.log(option);
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
     }
-    // if location is unknown getLocation returns undefined
-    // e.g. return undefined;
+
+    renderApp();
 }
 
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : "Anonymous"}!</h1>
-        {user.age && user.age >= 18 ? <p>Age: {user.age}</p> : false}
-        {getLocation(user.location)}
-    </div>
-);
+function removeAll(e) {
+    app.options = [];
+
+    renderApp();
+}
 
 const appRoot = document.getElementById('app');
 
-let count = 0;
-
-const addOne = () => {
-    console.log("addOne");
-    count++;
-    renderCounterApp();
-};
-
-function minusOne() {
-    console.log("minusOne");
-    count--;
-    renderCounterApp();
+const renderApp = () => {
+    const template = (<div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {renderOptions(app.options)}
+        <form onSubmit={onFormSubmit}>
+            <input type="text" name="option"/>
+            <button>Add Option</button>
+        </form>
+    </div>)
+    ReactDOM.render(template, appRoot);
 }
 
-function reset() {
-    console.log("reset");
-    count = 0;
-    renderCounterApp();
-}
-
-const renderCounterApp = () => {
-    const templateThree = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
-
-    ReactDOM.render(templateThree, appRoot);
-};
-
-renderCounterApp();
-
+renderApp();
