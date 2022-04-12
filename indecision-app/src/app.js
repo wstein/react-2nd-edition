dfec4a418a2d628c57b21d68974780db52469410
Werner Stein <werner.stein@gmail.com>
2022-04-12 21:58:54 +0200

    20. Picking an Option

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -7,19 +7,25 @@ const app = {
     options: []
 }
 
+function onMakeDecision() {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+}
+
 function renderOptions(options) {
     if (options && options.length > 0) {
-        return (<div>
-            <p>Here are your options: </p>
-            <button onClick={removeAll}>Remove All</button>
-            <ol>
-                {options.map((option, id) =>
-                    <li key={id}>Item {option}</li>
-                )}
-            </ol>
-        </div>);
+        return (
+            <div>
+                <p>Here are your options: </p>
+                <ol>
+                    {options.map((option, id) =>
+                        <li key={id}>Item {option}</li>
+                    )}
+                </ol>
+            </div>);
     } else {
-        return <p>No options</p>
+        return <p>No options!</p>
     }
 }
 
@@ -47,6 +53,8 @@ const appRoot = document.getElementById('app');
 const renderApp = () => {
     const template = (<div>
         <h1>{app.title}</h1>
+        <button disabled={!app.options.length} onClick={onMakeDecision}>What should I do?</button>
+        <button disabled={!app.options.length} onClick={removeAll}>Remove All</button>
         {app.subtitle && <p>{app.subtitle}</p>}
         {renderOptions(app.options)}
         <form onSubmit={onFormSubmit}>
