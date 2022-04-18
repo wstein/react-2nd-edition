5e2fb629941f2a722a74061318bef075ce3a61e4
Werner Stein <werner.stein@gmail.com>
2022-04-18 21:51:23 +0200

    54. Setting up Babel with Webpack

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,6 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = React.createElement('p', {}, 'testing 123...');
+const template = <p>This is JSX from WebPack...</p>
 
 ReactDOM.render(template, document.getElementById('app'));
