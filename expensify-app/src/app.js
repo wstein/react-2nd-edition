103309365634a4f26859cbed1e3d499cb279d6ce
Werner Stein <werner.stein@gmail.com>
2022-04-24 22:37:12 +0200

    80. Organizing Our Routes

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,65 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Link, NavLink, Route, Switch} from 'react-router-dom';
+
+import AppRouter from "./routers/AppRouter";
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from my dashboard component.
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is from my add expense component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        This is from my edit expense component
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is from my help component
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404 - <Link to="/">Go home</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <ul>
-            <li><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></li>
-            <li><NavLink to="/create" activeClassName="is-active">Create Expense</NavLink></li>
-            <li><NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink></li>
-            <li><NavLink to="/help" activeClassName="is-active">Help</NavLink></li>
-        </ul>
-    </header>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header/>
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-                <Route path="/create" component={AddExpensePage}/>
-                <Route path="/edit" component={EditExpensePage}/>
-                <Route path="/help" component={HelpPage}/>
-                <Route component={NotFoundPage}/>
-            </Switch>
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
