8cb9e85398aa3599630d14e016e6ffa9e5fa1ace
Werner Stein <werner.stein@gmail.com>
2022-04-25 21:28:58 +0200

    86. Dispatching Actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -4,6 +4,46 @@ let initState = {
     count: 0
 };
 
-const store = createStore((state = initState) => state);
+const store = createStore((state = initState, action) => {
+    switch (action.type) {
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default:
+            return state;
+    }
+});
+
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'RESET'
+});
+
+console.log(store.getState());
+
+store.dispatch({
+    type: 'DECREMENT'
+});
 
 console.log(store.getState());
