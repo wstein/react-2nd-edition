2fc14139c9308189d73f8f23d7c25051c82ed37e
Werner Stein <werner.stein@gmail.com>
2022-04-28 23:14:38 +0200

    89. ES6 Array Destructuring

--- expensify-app/src/playground/destructoring.js
+++ expensify-app/src/playground/destructoring.js
@@ -1,4 +1,6 @@
-const person = {
+// Object
+{
+    const person = {
     name: 'Werner',
     age: 54,
     location: {
@@ -7,10 +9,20 @@ const person = {
     }
 };
 
-const {name: firstName = 'Anonymous', age} = person;
+    const {name: firstName = 'Anonymous', age} = person;
 
-console.log(`${firstName} is ${age}`);
+    console.log(`${firstName} is ${age}`);
 
-const {city, temp: temperature} = person.location || {};
+    const {city, temp: temperature} = person.location || {};
 
-city && temperature && console.log(`It is ${temperature} in ${city}`);
+    city && temperature && console.log(`It is ${temperature} in ${city}`);
+}
+
+// Array
+{
+    const address = ['Hans Str 11', 'Stuttgart', 'BW', 70469];
+
+    const [, city, state, , country = 'Germany'] = address;
+    console.log(`You are in ${address[1]} ${address[2]}`);
+    console.log(`You are in ${city} ${state} ${country}`);
+}
